05e45c7a7b3d6b1643f54352e709961ecd4fdeaa
Atexide Fabrice <navyblu971@hotmail.com>
2016-05-05 12:17:33 +0200

    ajout light et humidité

--- hydroponic.ino
+++ hydroponic.ino
@@ -11,14 +11,30 @@ lcd13D6 = 3, // Arduino Pin 3.
 lcd14D7 = 2; // Arduino Pin 2.
 LiquidCrystal lcd(lcd4RS, lcd6E, lcd11D4, lcd12D5, lcd13D6, lcd14D7);
 int potOut;
+
+//sensor d'humidité..
+int waterSensor = A0;    // select the input pin for the potentiometer
+int waterValue = 0;  // variable to store the value coming from the sensor
+
+int lightSensor = A1 ; 
+int lightValue = 0 ; 
+
+int temperature = A3 ; 
+int temValue = 0 ; 
+
 void setup() {
  Serial.begin(9600);
  // set up the LCD's number of rows and columns: 
  lcd.begin(16, 2);
  // Print a message to the LCD.
  lcd.setCursor(0, 0);  
- lcd.print("hello, world!");
+ lcd.print("      Eau    ");
+ //lcd.print("Tx D'humidité : ");
  Serial.println("Welcome to Lesson 19: LED Proof.");
+
+  
+
+ 
 }
 
 void loop() {
@@ -30,7 +46,44 @@ Serial.print("  Pot Output = "); Serial.println(potOut);
  // (note: line 1 is the second row, since counting begins with 0):
  lcd.setCursor(0, 1);
  // print the number of seconds since reset:
- lcd.print(millis()/1000);
-delay(1000);
+
+
+ // humidite() ; 
+ light() ; 
+
+  delay(1000);
 }
+
+
+
+void light ()
+{
+
+ lightValue = analogRead(lightSensor);    
+ lcd.setCursor(0, 0);  
+ lcd.print("      Lumiere   ");  
+ lcd.setCursor(0, 1);
+ lcd.print("      ") ; 
+ lcd.print(lightValue); 
+
+}
+
+void humidite ()
+{
+ // read the value from the sensor:
+  waterValue = 1024 - analogRead(waterSensor);        
+  Serial.print("sensor = " );  
+  lcd.setCursor(0, 0);  
+  lcd.print("      eau   ");                       
+   Serial.println(waterValue); 
+    lcd.setCursor(0, 1);   
+   lcd.print("      "); 
+   lcd.print(waterValue); 
+   lcd.print("%     ");   
+ 
+
+}
+
+
+
 
